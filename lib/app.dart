aa067fb7aa012ed79e4f5c35151492c207f2e91d
Erik Pizana <erikpzcanedo@outlook.com>
2021-02-03 14:40:00 -0600

    First commit

---
+++ lib/app.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:components_app/pages/home_page.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return HomePage();
+  }
+}
